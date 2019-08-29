5d13d44b1b6e1acdd9bdf6e7107af6f132316e85
zyoctopus <530585198@qq.com>
2019-08-29 11:48:16 +0800

    修改

--- getNovel/biquge/index.py
+++ getNovel/biquge/index.py
@@ -1,56 +1,68 @@
 # -*- coding: utf-8 -*
 import re
 import os
+import time
 import json
 import requests
 from bs4 import BeautifulSoup
 from common import getUrlContent, getNovelUrlByTitle, getNovelUrlListById, getNovelContent
 
-with open('./config.json') as configfile:
-    data = configfile.read()
+def main(defaultIndex=0):
 
-pwd = os.getcwd()
+    try:
 
-# 获取配置
-configData = json.loads(data)
+        with open('./config.json') as configfile:
+            data = configfile.read()
 
-# 获取搜索的小说列表
-content = getUrlContent(configData['url'], configData['params'])
+        pwd = os.getcwd()
 
-# 获取小说所在的地址
-novelUrl = getNovelUrlByTitle(content, configData['params']['keyword'])
+        # 获取配置
+        configData = json.loads(data)
 
-# 获取小说网页内容
-content = getUrlContent(novelUrl)
+        # 获取搜索的小说列表
+        content = getUrlContent(configData['url'], configData['params'])
 
-# 获取所有章节的地址
-sectionUrlList = getNovelUrlListById(content, "list")
+        # 获取小说所在的地址
+        novelUrl = getNovelUrlByTitle(content, configData['params']['keyword'])
 
-# 检测文件夹是否存在
-dirPath = pwd + "/" + configData['params']['keyword']
-folder = os.path.exists(dirPath)
-if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
-    os.makedirs(dirPath)            #makedirs 创建文件时如果路径不存在会创建这个路径
-    print("---  new folder...  ---")
+        # 获取小说网页内容
+        content = getUrlContent(novelUrl)
 
-content = getUrlContent(configData['domain'] + sectionUrlList[1])
-text, title = getNovelContent(content)
+        # 获取所有章节的地址
+        sectionUrlList = getNovelUrlListById(content, "list")
 
-filename = ''
-for index in range(len(sectionUrlList)):
-    model = 'a'
-    # if(index == 3): # 测试三章用的
-    #     break
+        # 检测文件夹是否存在
+        dirPath = pwd + "/" + configData['params']['keyword']
+        folder = os.path.exists(dirPath)
+        if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
+            os.makedirs(dirPath)  # makedirs 创建文件时如果路径不存在会创建这个路径
+            print("---  new folder...  ---")
 
-    if(index % 100 == 0):
-        filename = '/novel_' + str(index) + '.txt'
-
-    if(index == 0):
-        model = 'w'
-
-    with open(dirPath + filename, model) as txt:
-        content = getUrlContent(configData['domain'] + sectionUrlList[index])
+        content = getUrlContent(configData['domain'] + sectionUrlList[1])
         text, title = getNovelContent(content)
-        text = re.sub(r"\s+", '\r\n', text)
-        txt.write('#### {title}\n{text}\n\n'.format(title=title, text=text))
 
+        for index in range(len(sectionUrlList)):
+            if(index < defaultIndex):
+                continue
+
+            filename = '/novel_' + str(int(index/100)) + '.txt'
+
+            model = 'a'
+            if(index == 0):
+                model = 'w'
+
+            with open(dirPath + filename, model) as txt:
+                content = getUrlContent(configData['domain'] + sectionUrlList[index])
+                text, title = getNovelContent(content)
+                text = re.sub(r"\s+", '\r\n', text)
+                txt.write('#### {title}\n{text}\n\n'.format(
+                    title=title, text=text))
+                time.sleep(0.1) # 延迟一会，不要太频繁
+
+    except Exception as reason:
+        print("发生了错误", reason, index)
+        main(index)  # 再次尝试执行
+
+
+if __name__ == '__main__':
+    main()
